730b950629747471f53b8a822a569152585167d0
Javier Bullrich <javier@bullrich.dev>
2023-09-26 18:36:37 +0200

    added missing outputs

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,12 @@ const logger = generateCoreLogger();
 const mapFellows = (fellows: Map<string, number>) => {
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const fellowsHandles = Array.from(fellows.keys());
-  setOutput("fellow-handles", JSON.stringify(fellowsHandles));
+  setOutput("fellows-handles", JSON.stringify(fellowsHandles));
   const fellowsMap = [...fellows].map(([name, rank]) => {
     return { user: name, rank };
   });
   setOutput("fellows-map", JSON.stringify(fellowsMap));
+  setOutput("fellows", fellowsHandles.join(","));
 };
 
 fetchAllFellows(logger).then(mapFellows).catch(setFailed);
