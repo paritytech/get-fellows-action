bbef0e2c671ce8b5ada73be86ec8276a8c8a19cd
Javier Bullrich <javier@bullrich.dev>
2024-03-15 16:06:35 +0100

    added summary to action
    Now it write a nice table with all the fellows sorted by rank

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
-import { setFailed, setOutput } from "@actions/core";
+import { setFailed, setOutput, summary } from "@actions/core";
 
 import { FellowObject, fetchAllFellows } from "./fellows";
 import { generateCoreLogger } from "./util";
+import { SummaryTableRow } from "@actions/core/lib/summary";
 
 const logger = generateCoreLogger();
 
-const mapFellows = (fellows: FellowObject[]) => {
+const mapFellows = async (fellows: FellowObject[]) => {
   setOutput("fellows", JSON.stringify(fellows));
   const githubHandles = fellows
     .map((f) => f.githubHandle)
     .filter((f) => !!f)
     .join(",");
   setOutput("github-handles", githubHandles);
+
+
+  const table:SummaryTableRow[] = [[{header: true, data:"Address", }, {header:true,data:"GitHub Handle"}, {header:true, data:"Rank"}]];
+
+  for (const fellow of fellows.sort(f => f.rank)){
+    table.push([fellow.address, `@${fellow.githubHandle}`, `\`fellow.rank\``]);
+  }
+
+  await summary.addHeading("Fellows")
+  .addTable(table)
+  .write();
 };
 
 fetchAllFellows(logger)
