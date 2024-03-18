d1ee5d0a77685cc679edeb205c9ebfd898adcbfa
Javier Bullrich <javier@bullrich.dev>
2024-03-18 11:10:43 +0100

    Added summary to action and fixed exit error (#17)
    - Added a nice summary that is shown after every run.
    - Made exiting of application conditional on the state.
    - With the finally it would always exit as successful even if it failed.
      - Now it sets an error code when valid.
    - Implemented action on itself.
      - Now it will run after each push.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,46 @@
-import { setFailed, setOutput } from "@actions/core";
+import { setFailed, setOutput, summary } from "@actions/core";
+import { SummaryTableRow } from "@actions/core/lib/summary";
 
 import { FellowObject, fetchAllFellows } from "./fellows";
 import { generateCoreLogger } from "./util";
 
 const logger = generateCoreLogger();
 
-const mapFellows = (fellows: FellowObject[]) => {
+const mapFellows = async (fellows: FellowObject[]) => {
   setOutput("fellows", JSON.stringify(fellows));
   const githubHandles = fellows
-    .map((f) => f.githubHandle)
-    .filter((f) => !!f)
+    .map(({ githubHandle }) => githubHandle)
+    .filter((handle) => !!handle)
     .join(",");
   setOutput("github-handles", githubHandles);
+
+  const table: SummaryTableRow[] = [
+    [
+      { header: true, data: "Rank" },
+      { header: true, data: "GitHub Handle" },
+      { header: true, data: "Address" },
+    ],
+  ];
+
+  for (const fellow of fellows.sort((old, { rank }) => rank - old.rank)) {
+    if (fellow.githubHandle) {
+      table.push([
+        fellow.rank.toString(),
+        `@${fellow.githubHandle}`,
+        fellow.address,
+      ]);
+    }
+  }
+
+  await summary.addHeading("Fellows").addTable(table).write();
+
+  // TODO: Remove this once https://github.com/polkadot-api/polkadot-api/issues/327 is fixed
+  process.exit(0);
 };
 
 fetchAllFellows(logger)
   .then(mapFellows)
-  .catch(setFailed)
-  .finally(() => {
-    logger.info("Shutting down application");
-    // TODO: Remove this once https://github.com/polkadot-api/polkadot-api/issues/327 is fixed
-    process.exit(0);
+  .catch((err) => {
+    setFailed(err as Error);
+    process.exit(1);
   });
