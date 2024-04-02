451080cdd28a450e3bccf57136c68d8b5b371708
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-04-02 10:27:34 +0200

    update PAPI and improve fellows-action (#20)

--- src/fellows.ts
+++ src/fellows.ts
@@ -1,4 +1,4 @@
-import { createClient, PolkadotClient } from "@polkadot-api/client";
+import { createClient, SS58String } from "@polkadot-api/client";
 import { getChain } from "@polkadot-api/node-polkadot-provider";
 import { getSmProvider } from "@polkadot-api/sm-provider";
 import {
@@ -25,132 +25,86 @@ export const fetchAllFellows = async (
   logger.info("Initializing smoldot");
   const smoldot = start();
 
-  let polkadotClient: PolkadotClient | null = null;
-
   try {
     const relayChain = await smoldot.addChain({
       chainSpec: polkadot,
-    });
-    logger.debug("Connecting to collective parachain");
-    await smoldot.addChain({
-      chainSpec: polkadot_collectives,
-      potentialRelayChains: [relayChain],
+      disableJsonRpc: true,
     });
 
-    const SmProviderCollectives = getSmProvider(smoldot, {
-      potentialRelayChains: [relayChain],
-      chainSpec: polkadot_collectives,
-    });
-    logger.info("Initializing PAPI");
-    polkadotClient = createClient(
+    logger.info("Initializing the relay client");
+    const relayClient = createClient(
       getChain({
-        provider: SmProviderCollectives,
+        provider: getSmProvider(smoldot, polkadot),
         keyring: [],
       }),
     );
+    const relayApi = relayClient.getTypedApi(relayDescriptor);
 
-    // We fetch all the members from the collective
-    const collectivesApi = polkadotClient.getTypedApi(collectiveDescriptor);
+    const getGhHandle = async (
+      address: SS58String,
+    ): Promise<string | undefined> => {
+      const identity =
+        await relayApi.query.Identity.IdentityOf.getValue(address);
+
+      if (identity)
+        return identity.info.additional
+          .find(([key]) => key.value?.asText() === "github")?.[1]
+          .value?.asText()
+          .replace("@", "");
+
+      const superIdentityAddress = (
+        await relayApi.query.Identity.SuperOf.getValue(address)
+      )?.[0];
+      return await (superIdentityAddress && getGhHandle(superIdentityAddress));
+    };
+
+    logger.info("Initializing the collectives client");
+    const collectivesClient = createClient(
+      getChain({
+        provider: getSmProvider(smoldot, {
+          potentialRelayChains: [relayChain],
+          chainSpec: polkadot_collectives,
+        }),
+        keyring: [],
+      }),
+    );
+    const collectivesApi = collectivesClient.getTypedApi(collectiveDescriptor);
+
+    // Pull the members of the FellowshipCollective
     const memberEntries =
       await collectivesApi.query.FellowshipCollective.Members.getEntries();
 
-    // We iterate over the fellow data and convert them into usable values
-    const fellows: FellowData[] = [];
-    for (const member of memberEntries) {
-      // We filter candidates (who are rank 0)
-      if (member.value > 0) {
-        const [address] = member.keyArgs;
-        fellows.push({ address, rank: member.value });
-      }
-    }
+    // Build the Array of FellowData and filter out candidates (zero rank members)
+    const fellows: FellowData[] = memberEntries
+      .map(({ keyArgs: [address], value: rank }) => {
+        return { address, rank };
+      })
+      .filter(({ rank }) => rank > 0);
     logger.debug(JSON.stringify(fellows));
 
-    // Once we obtained this information, we disconnect this api.
-    polkadotClient.destroy();
+    // We no longer need the collectives client, so let's destroy it
+    collectivesClient.destroy();
 
-    logger.debug("Connecting to relay parachain.");
-
-    // We move into the relay chain
-    const SmProviderRelay = getSmProvider(smoldot, {
-      potentialRelayChains: [relayChain],
-      chainSpec: polkadot,
-    });
-    polkadotClient = createClient(
-      getChain({
-        provider: SmProviderRelay,
-        keyring: [],
+    // Let's now pull the GH handles of the fellows
+    const users: FellowObject[] = await Promise.all(
+      fellows.map(async ({ address, rank }) => {
+        return {
+          address,
+          rank,
+          githubHandle: await getGhHandle(address),
+        };
       }),
     );
-    const relayApi = polkadotClient.getTypedApi(relayDescriptor);
-
-    const users: FellowObject[] = [];
-
-    // We iterate over the different members and extract their data
-    for (const fellow of fellows) {
-      logger.debug(
-        `Fetching identity of '${fellow.address}', rank: ${fellow.rank}`,
-      );
-
-      const fellowData = await relayApi.query.Identity.IdentityOf.getValue(
-        fellow.address,
-      );
-
-      let data: FellowObject = { address: fellow.address, rank: fellow.rank };
-
-      // If the identity is null, we check if there is a super identity.
-      if (!fellowData) {
-        logger.debug("Identity is null. Checking for super identity");
-        const superIdentity = await relayApi.query.Identity.SuperOf.getValue(
-          fellow.address,
-        );
-        if (superIdentity) {
-          const [address] = superIdentity;
-          logger.debug(
-            `${fellow.address} has a super identity: ${address}. Adding it to the array`,
-          );
-
-          fellows.push({ address, rank: fellow.rank });
-        } else {
-          logger.debug("No super identity found. Skipping");
-        }
-
-        continue;
-      }
-
-      const additional = fellowData.info.additional;
-
-      // If it does not have additional data (GitHub handle goes here) we ignore it
-      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-      if (!additional || additional.length < 1) {
-        logger.debug("Additional data is null. Skipping");
-        continue;
-      }
-
-      for (const additionalData of additional) {
-        const [key, value] = additionalData;
-        // We verify that they have an additional data of the key "github"
-        // If it has a handle defined, we push it into the array
-        const fieldName = key.value?.asText();
-        logger.debug(`Analyzing: ${fieldName ?? "unknown field"}`);
-        const fieldValue = value.value?.asText();
-        if (fieldName === "github" && fieldValue && fieldValue.length > 0) {
-          logger.debug(`Found handles: '${fieldValue}`);
-          // We add it to the array and remove the @ if they add it to the handle
-          data = { ...data, githubHandle: fieldValue.replace("@", "") };
-        }
-        users.push(data);
-      }
-    }
     logger.info(`Found users: ${JSON.stringify(Array.from(users.entries()))}`);
+
+    // We are now done with the relay client
+    relayClient.destroy();
 
     return users;
   } catch (error) {
     logger.error(error as Error);
     throw error;
   } finally {
-    if (polkadotClient) {
-      polkadotClient.destroy();
-    }
     await smoldot.terminate();
   }
 };
