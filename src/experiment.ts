a9e9c6002f57d799c4d36f393210d0f2bdf9d048
Javier Bullrich <javier@bullrich.dev>
2024-04-23 11:42:40 +0200

    created a simple experiment

---
+++ src/experiment.ts
@@ -0,0 +1,42 @@
+import { polkadot as polkadot_descriptor } from "@polkadot-api/descriptors";
+import { createClient } from "polkadot-api";
+import polkadot_chain, {
+    chainSpec as polkadotChainSpec,
+} from "polkadot-api/chains/polkadot";
+import { getSmProvider } from "polkadot-api/sm-provider";
+import { start } from "smoldot";
+
+import { ActionLogger } from "./github/types";
+
+export const fetchIdentity = async (logger: ActionLogger): Promise<any> => {
+    console.log("polkadot chain", polkadot_chain);
+    logger.info("Initializing smoldot");
+    const smoldot = start();
+
+    try {
+        const smoldotRelayChain = await smoldot.addChain({
+            chainSpec: polkadotChainSpec,
+            disableJsonRpc: true,
+        });
+
+        const jsonRpcProvider = getSmProvider(smoldotRelayChain);
+        logger.info("Initializing the relay client");
+        const polkadotClient = createClient(jsonRpcProvider);
+
+        const relayApi = polkadotClient.getTypedApi(polkadot_descriptor);
+
+        const identity = await relayApi.query.Identity.IdentityOf.getValue(
+            "1eTPAR2TuqLyidmPT9rMmuycHVm9s9czu78sePqg2KHMDrE",
+        );
+
+        console.log("Identity is", identity);
+
+        return identity;
+    } catch (error) {
+        console.warn("something went wrong");
+        logger.error(error as Error);
+        throw error;
+    } finally {
+        await smoldot.terminate();
+    }
+};
