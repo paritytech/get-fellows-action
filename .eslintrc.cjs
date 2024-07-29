84e822f3b173e4d39c6fe97401ad4b93f66564a4
cornholio <0@mcornholio.ru>
2024-07-29 16:53:16 +0200

    Test bringing codestyle rules back

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,8 +8,6 @@ const tsConfParams = { rootDir: __dirname };
 const conf = getConfiguration({ typescript: tsConfParams });
 
 const tsConfOverride = getTypescriptOverride(tsConfParams);
-tsConfOverride.rules["@typescript-eslint/strict-boolean-expressions"] = 0
-tsConfOverride.rules["@typescript-eslint/no-unsafe-call"] = 0
 conf.overrides.push(tsConfOverride);
 
 module.exports = conf;
